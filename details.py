fd9f02903233fff990a0b401685757d5f3889235
monkeydluffy76 <124869601+monkeydluffy76@users.noreply.github.com>
2023-10-20 01:03:11 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,7 +1,7 @@
-api_id = 
-api_hash = ""
-bot_token = ""
-auth_users = []
-sudo_user = 
-log_channel = 
-txt_channel = 
+api_id = 9541960
+api_hash = "17a1aa509b35564481dae98d89d0191d"
+bot_token = "6610143533:AAFvYZMQtcJeFenFlOAVTRP1pMo8F_f47HI"
+auth_users = [942518345]
+sudo_user = 942518345
+log_channel = -1001780851786
+txt_channel = -1001780851786
