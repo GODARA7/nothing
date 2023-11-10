4ebcea5d85cc525f116ea164c5c93f12132ac398
monkeydluffy76 <124869601+monkeydluffy76@users.noreply.github.com>
2023-11-10 23:21:13 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,7 +1,4 @@
 api_id = 9541960
 api_hash = "17a1aa509b35564481dae98d89d0191d"
-bot_token = "6610143533:AAFvYZMQtcJeFenFlOAVTRP1pMo8F_f47HI"
-auth_users = [942518345]
-sudo_user = 942518345
-log_channel = -1001780851786
-txt_channel = -1001780851786
+bot_token = "6435141772:AAHynl9w9v1om4J_sBsOmIklRI4-aO1UY7Y"
+sudo_user = -1001912090653
