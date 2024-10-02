4abcebd3b54c99e2fffb03ceb8121f258d3668ad
photon256 <144157217+photon256@users.noreply.github.com>
2024-10-02 22:37:16 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = "9541960"
-api_hash = "17a1aa509b35564481dae98d89d0191d"
-bot_token = "6435141772:AAHynl9w9v1om4J_sBsOmIklRI4-aO1UY7Y"
-sudo_groups = -866289873
+api_id = "16253557"
+api_hash = "81171c25e4cb9062cb10da8b7730432a"
+bot_token = "6722382064:AAES1OLvEfL5HLTgXNZDsgpSrFWFfgmHuV8"
+sudo_groups = -1002470842791
